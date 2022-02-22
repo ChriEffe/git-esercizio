5463347bb94a7ceb5de93cab74300824ba9564f7
furvietto <fulvio-94@hotmail.it>
2022-02-22 13:46:29 +0100

    remove function

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -27,59 +27,7 @@ class HouseController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\House  $house
-     * @return \Illuminate\Http\Response
-     */
-    public function show(House $house)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\House  $house
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(House $house)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\House  $house
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, House $house)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\House  $house
-     * @return \Illuminate\Http\Response
-     */
-    // public function destroy(House $house)
-    // {
-    //     //
-    // }
+   
 }
